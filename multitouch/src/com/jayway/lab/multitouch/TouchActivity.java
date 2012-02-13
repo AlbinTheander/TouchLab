c1cfca1d5a3fa9a87523d6dd55e394bc8dde7d0f
Albin Theander <Albin.Theander@jayway.com>
2012-02-13 09:08:45 +0100

    Added scaling and rotation

--- multitouch/src/com/jayway/lab/multitouch/TouchActivity.java
+++ multitouch/src/com/jayway/lab/multitouch/TouchActivity.java
@@ -3,6 +3,7 @@ package com.jayway.lab.multitouch;
 import android.app.Activity;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
@@ -16,6 +17,10 @@ public class TouchActivity extends Activity {
 	private Matrix imageMatrix = new Matrix();
 
 	private int fingersUsed;
+
+	private float savedDistance;
+
+	private float savedAngle;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,11 +42,13 @@ public class TouchActivity extends Activity {
 		case MotionEvent.ACTION_POINTER_DOWN:
 			savedX = (event.getX(0) + event.getX(1)) / 2;
 			savedY = (event.getY(0) + event.getY(1)) / 2;
-		savedMatrix.set(imageView.getImageMatrix());
+			savedDistance = distance(event);
+			savedAngle = angle(event);
+			savedMatrix.set(imageView.getImageMatrix());
 			fingersUsed = 2;
 			break;
 		case MotionEvent.ACTION_UP:
-	case MotionEvent.ACTION_POINTER_UP:
+		case MotionEvent.ACTION_POINTER_UP:
 			fingersUsed = 0;
 			break;
 		case MotionEvent.ACTION_MOVE:
@@ -57,8 +64,13 @@ public class TouchActivity extends Activity {
 				float midY = (event.getY(0) + event.getY(1)) / 2;
 				float dx = midX - savedX;
 				float dy = midY - savedY;
+				float newDistance = distance(event);
+				float newAngle = angle(event);
+				float scale = newDistance / savedDistance;
 				imageMatrix.set(savedMatrix);
 				imageMatrix.postTranslate(dx, dy);
+				imageMatrix.postScale(scale, scale, midX, midY);
+				imageMatrix.postRotate(savedAngle - newAngle, midX, midY);
 			}
 			break;
 		default:
@@ -66,5 +78,29 @@ public class TouchActivity extends Activity {
 		}
 		imageView.setImageMatrix(imageMatrix);
 		return true;
+	}
+
+	/**
+	 * returns the distance between the two first pointers in the event
+	 * 
+	 * @return the distance
+	 */
+	private float distance(MotionEvent event) {
+		float xDiff = event.getX(1) - event.getX(0);
+		float yDiff = event.getY(1) - event.getY(0);
+		return FloatMath.sqrt(xDiff * xDiff + yDiff * yDiff);
+	}
+
+	/**
+	 * returns the angle in degrees of the line going from the first pointer to
+	 * the second pointer in the MotionEvent.
+	 * 
+	 * @return the angle in degrees
+	 */
+	private float angle(MotionEvent event) {
+		float xDiff = event.getX(1) - event.getX(0);
+		float yDiff = event.getY(1) - event.getY(0);
+		double radianAngle = Math.atan2(xDiff, yDiff);
+		return (float) (radianAngle * 180 / Math.PI);
 	}
 }
