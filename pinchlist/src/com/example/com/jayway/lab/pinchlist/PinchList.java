ed89482262dd0eda65fff4384d342d503a15a1c0
Albin Theander <Albin.Theander@jayway.com>
2013-01-31 11:20:02 +0100

    Added solution to PinchList

--- pinchlist/src/com/example/com/jayway/lab/pinchlist/PinchList.java
+++ pinchlist/src/com/example/com/jayway/lab/pinchlist/PinchList.java
@@ -2,6 +2,7 @@ package com.example.com.jayway.lab.pinchlist;
 
 import android.app.ExpandableListActivity;
 import android.os.Bundle;
+import android.util.FloatMath;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -10,6 +11,9 @@ import android.widget.ExpandableListView;
 
 public class PinchList extends ExpandableListActivity implements
 		OnTouchListener {
+
+	private boolean twoFingersDown;
+	private float savedDistance;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,34 @@ public class PinchList extends ExpandableListActivity implements
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		boolean handled = false;
+		switch (event.getActionMasked()) {
+		case MotionEvent.ACTION_DOWN:
+			break;
+		case MotionEvent.ACTION_POINTER_DOWN:
+			twoFingersDown = true;
+			savedDistance = distance(event);
+			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_POINTER_UP:
+			twoFingersDown = false;
+			break;
+		case MotionEvent.ACTION_MOVE:
+			// Update values by comparing the current values to
+			// the saved ones
+			if (twoFingersDown) {
+				float newDistance = distance(event);
+				float distanceDiff = newDistance - savedDistance;
+				if (distanceDiff > 50) {
+					expandAll();
+				} else if (distanceDiff < -50)
+					collapseAll();
+				handled = true;
+			}
+			break;
+		default:
+			break;
+		}
+
 		if (handled) {
 			return true;
 		} else {
@@ -53,4 +85,16 @@ public class PinchList extends ExpandableListActivity implements
 			listView.expandGroup(i);
 		}
 	}
+
+	/**
+	 * returns the distance between the two first pointers in the event
+	 * 
+	 * @return the distance
+	 */
+	private float distance(MotionEvent event) {
+		float xDiff = event.getX(1) - event.getX(0);
+		float yDiff = event.getY(1) - event.getY(0);
+		return FloatMath.sqrt(xDiff * xDiff + yDiff * yDiff);
+	}
+
 }
